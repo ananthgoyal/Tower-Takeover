163b6d7f08d5e806e20f7c9f2d3237c23dca0215
Tanay Reddy <tanaykreddy@gmail.com>
2018-12-01 17:16:47 -0800

    Added Bang-bang controller
    bang-bang-bang-bang

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -15,46 +15,58 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-//void flywheelTask(void* param);
+
+okapi::Controller controller;
+
+void flywheelTask(void* param);
+
 void opcontrol() {
 
-	//std::string text("PROS");
-	//pros::Task my_task(flywheelTask, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "flywheelTask");
+	std::string text("PROS");
+	pros::Task my_task(flywheelTask, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "flywheelTask");
 
-	okapi::Controller controller;
-	okapi::Motor intake (8);
-	okapi::Motor indexer (-9);
-	okapi::Motor capFlipper(-3, false, okapi::AbstractMotor::gearset::red);
+	okapi::Motor indexer (9, true, okapi::AbstractMotor::gearset::red);
+	okapi::Motor capFlipper(3, true, okapi::AbstractMotor::gearset::red);
 
-	auto myChassis = okapi::ChassisControllerFactory::create(2, 10, 20, 11);
-
+	auto myChassis = okapi::ChassisControllerFactory::create({2, 11}, {10, 20});
 
 	while (true) {
 		myChassis.arcade(controller.getAnalog(okapi::ControllerAnalog::rightX), controller.getAnalog(okapi::ControllerAnalog::leftY));
-
-		intake.controllerSet(controller.getDigital(okapi::ControllerDigital::L2) - controller.getDigital(okapi::ControllerDigital::L1));
 		indexer.controllerSet(controller.getDigital(okapi::ControllerDigital::R2) - controller.getDigital(okapi::ControllerDigital::R1));
 		capFlipper.controllerSet(controller.getDigital(okapi::ControllerDigital::up) - controller.getDigital(okapi::ControllerDigital::down));
 
 		pros::delay(20);
 	}
-
 }
 
-/*
 void flywheelTask(void* param) {
-	okapi::Motor flywheel1(1, true, okapi::AbstractMotor::gearset::green);
-	okapi::Motor flywheel2(5, false, okapi::AbstractMotor::gearset::green);
-	//pros::Motor l_flywheel1(-1);
-	//pros::Motor l_flywheel2(5);
+	okapi::Motor flywheelTop(7, true, okapi::AbstractMotor::gearset::green);
+	okapi::Motor flywheelBot(5, false, okapi::AbstractMotor::gearset::green);
+	okapi::ADIEncoder encoder('H', 'G', false);
 
+	int targetRPM = 2000;
+	int curRPM;
+	encoder.reset();
   while (true) {
-    //l_flywheel1.move_voltage(12000);
-		//l_flywheel2.move_voltage(12000);
-		flywheel1.moveVelocity(200);
-		flywheel2.moveVelocity(200);
+		/*
+		curRPM = encoder.get() * 100/3;
+		encoder.reset();
 
+		if (targetRPM < curRPM) {
+			flywheelTop.moveVelocity(100);
+			flywheelBot.moveVelocity(100);
+		}
+		else {
+			flywheelTop.moveVelocity(200);
+			flywheelBot.moveVelocity(200);
+		}
+
+		//flywheel1.controllerSet(controller.getDigital(okapi::ControllerDigital::L2) - controller.getDigital(okapi::ControllerDigital::L1));
+		//flywheel2.controllerSet(controller.getDigital(okapi::ControllerDigital::L2) - controller.getDigital(okapi::ControllerDigital::L1));
+		*/
+		flywheelTop.moveVoltage(12000);
+		flywheelBot.moveVoltage(12000);
 		pros::delay(20);
+
   }
 }
-*/
